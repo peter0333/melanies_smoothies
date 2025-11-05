f50bcc88d9fab646c4f879b592dab8d6d0a33930
peter0333 <155114270+peter0333@users.noreply.github.com>
2025-11-05 11:24:25 +0100

    Update streamlit_app.py
    Antoher fix to stretch

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf.df = st.dataframe(data=smoothiefroot_response.json(), width=stretch)
+sf.df = st.dataframe(data=smoothiefroot_response.json(), width='stretch')
